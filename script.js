bbb9fc3b25da7d09ad1ef40d995969663a224279
Oveena Widyaratne <oveena.w@gmail.com>
2025-04-19 16:15:17 +0930

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~";
+    let password = "";
+  
+    for (let i = 0; i < length; i++) {
+      const randomChar = charset[Math.floor(Math.random() * charset.length)];
+      password += randomChar;
+    }
+  
+    document.getElementById("password").value = password;
+  }
+  
+  function copyPassword() {
+    const passwordField = document.getElementById("password");
+    passwordField.select();
+    document.execCommand("copy");
+    alert("Password copied to clipboard!");
+  }
